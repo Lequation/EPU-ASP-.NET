ba30ae1bf31e854d577972125d28a53b088c2e5d
DESKTOP-4GGU9PJ\A42F <cuongdd@epu.edu.vn>
2017-12-20 11:15:20 +0700

    commit all

--- EPUWeb/Areas/Admin/Controllers/UserController.cs
+++ EPUWeb/Areas/Admin/Controllers/UserController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Models;
 
 namespace EPUWeb.Areas.Admin.Controllers
 {
@@ -22,20 +21,6 @@ namespace EPUWeb.Areas.Admin.Controllers
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(LoginModel model)
-        {
-            var result = new AccountService().Login(model.UserName, model.Password);
-            if(result && ModelState.IsValid)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                ModelState.AddModelError("", "Đăng nhập thất bại");
-            }
-            return View(model);
-        }
+        
     }
 }
